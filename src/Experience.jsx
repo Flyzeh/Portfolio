313bec81219d4b26902c5b18008f9df2231b3fef
Flyzeh <giani.negroni@outlook.fr>
2025-04-14 17:23:40 +0200

    Update

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { Environment, useGLTF, OrbitControls } from '@react-three/drei'
+import { Float, Environment, useGLTF, PresentationControls} from '@react-three/drei'
 
 export default function Experience()
 {
@@ -12,10 +12,21 @@ export default function Experience()
 
         <color args ={['#241a1a']} attach="background" />
 
-        <OrbitControls makeDefault />
-
-        <primitive object={computer.scene}/>
-
+        <PresentationControls
+            global
+            rotation={ [0.13, 0.1, 0] }
+            polar={ [-0.4, 0.2] }
+            azimuth={ [-1, 0.75] }
+            config={ {mass: 2, tension: 400} }
+            snap
+        >
+            <Float rotationIntensity={0.4}>
+            <primitive 
+                object={computer.scene}
+                position-y={-1.2}
+            />
+            </Float>
+        </PresentationControls>
     </>
 }
 
