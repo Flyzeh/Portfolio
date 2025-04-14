f32824fc65ca3536ca315dec4614254e309dffeb
Flyzeh <giani.negroni@outlook.fr>
2025-04-14 21:13:57 +0200

    Shadow / Light and Iframe

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { Float, Environment, useGLTF, PresentationControls} from '@react-three/drei'
+import { Text, Html, Float, Environment, useGLTF, PresentationControls, ContactShadows} from '@react-three/drei'
 
 export default function Experience()
 {
@@ -21,12 +21,45 @@ export default function Experience()
             snap
         >
             <Float rotationIntensity={0.4}>
-            <primitive 
-                object={computer.scene}
-                position-y={-1.2}
-            />
+                <rectAreaLight
+                    width={2.5}
+                    height={1.65}
+                    intensity={65}
+                    color={'#ff6900'}
+                    rotation={[0.1, Math.PI, 0]}
+                    position={[0, 0.55, -1.15]}
+                />
+                <primitive 
+                    object={computer.scene}
+                    position-y={-1.2}
+                >
+                    <Html
+                        transform
+                        wrapperClass='htmlScreen'
+                        distanceFactor={1.17}
+                        position={[0, 1.56, -1.4]}
+                        rotation-x={- 0.256}
+                    >
+                        <iframe src="" scrolling='no' ></iframe>
+                    </Html>
+                </primitive>
+                <Text
+                    font='./bangers-v20-latin-regular.woff'
+                    fontSize={1}
+                    position={[2, 0.75, 0.75]}
+                    rotation-y={-1.25}
+                    maxWidth={2}
+                    textAlign="center"
+                >GIANI NEGRONI</Text>
             </Float>
         </PresentationControls>
+
+        <ContactShadows 
+            position-y={-1.4} 
+            opacity={0.4}
+            scale={5}
+            blur={2.4}
+        />
     </>
 }
 
