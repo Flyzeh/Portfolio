8f9e7a45e148600ec6ec4e2fba57dbe564753209
Flyzeh <giani.negroni@outlook.fr>
2025-04-14 17:00:36 +0200

    test

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -13,11 +13,6 @@ export default function Experience()
         <color args ={['#241a1a']} attach="background" />
 
         <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
 
         <primitive object={computer.scene}/>
 
