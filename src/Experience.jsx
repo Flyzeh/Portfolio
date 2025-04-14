921b5d4a2c6d2ac2632a11f658553c9fca67bb86
Flyzeh <giani.negroni@outlook.fr>
2025-04-14 15:28:16 +0200

    start

---
+++ src/Experience.jsx
@@ -0,0 +1,26 @@
+import { Environment, useGLTF, OrbitControls } from '@react-three/drei'
+
+export default function Experience()
+{
+
+    const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf')  
+    console.log(computer)
+
+    return <>
+
+        <Environment preset="city"/>
+
+        <color args ={['#241a1a']} attach="background" />
+
+        <OrbitControls makeDefault />
+        
+        <mesh>
+            <boxGeometry />
+            <meshNormalMaterial />
+        </mesh>
+
+        <primitive object={computer.scene}/>
+
+    </>
+}
+
