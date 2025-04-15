57a5b4b0ccf39d3cd19d763269e4bbaec0d3756b
Flyzeh <giani.negroni@outlook.fr>
2025-04-15 12:57:08 +0200

    overflow

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -40,7 +40,7 @@ export default function Experience()
                         position={[0, 1.56, -1.4]}
                         rotation-x={- 0.256}
                     >
-                        <iframe src="https://giani-negroni-html.vercel.app/" scrolling='no' ></iframe>
+                        <iframe src="https://giani-negroni-html.vercel.app/" ></iframe>
                     </Html>
                 </primitive>
                 <Text
